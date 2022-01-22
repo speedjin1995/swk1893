a4fe9081ff1d012bc8d6d5325a5a1d587aa77305
Teoh Chia Jun <jeffreyteoh4311@gmail.com>
2022-01-22 22:11:06 +0800

    File resize and Logo
    - Removed unrelated files
    - Added icon for browser tab

--- admin/index.php
+++ admin/index.php
@@ -96,7 +96,7 @@ to get the desired effect
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="https://speedjin.com/durianduke" class="brand-link">
+    <a href="" class="brand-link">
       <img src="../images/logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Durian Duke</span>
     </a>
--- admin/php/db_connect.php
+++ admin/php/db_connect.php
@@ -1,6 +1,6 @@
 <?php
 date_default_timezone_set('Asia/Kuala_Lumpur');
-$db = mysqli_connect("localhost", "u782565293_durianduke", "s!mewk/!E9>", "u782565293_durianduke");
+$db = mysqli_connect("localhost", "root", "", "u782565293_durianduke");
 
 if(mysqli_connect_errno()){
     echo 'Database connection failed with following errors: ' . mysqli_connect_error();
