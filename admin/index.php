e7a5af40c530cf98812c1245071194e0f144b840
speedjin1995 <speedjin1995@gmail.com>
2022-01-24 14:54:39 +0800

    done admin
    Admin pages

--- admin/index.php
+++ admin/index.php
@@ -104,9 +104,9 @@ to get the desired effect
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="https://durianduke.com/" class="brand-link">
-      <img src="../images/logo.png" alt="Durian Duke Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">Durian Duke</span>
+    <a href="#" class="brand-link">
+      <img src="../assets/demo/logo.png" alt="Durian Duke Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">SWK</span>
     </a>
 
     <!-- Sidebar -->
@@ -160,12 +160,12 @@ to get the desired effect
               </p>
             </a>
             <ul class="nav nav-treeview">
-              <li class="nav-item">
+              <!--li class="nav-item">
                 <a href="profile.php" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Update Profile</p>
                 </a>
-              </li>
+              </li-->
               <li class="nav-item">
                 <a href="changepassword.php" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
--- admin/message.php
+++ admin/message.php
@@ -104,12 +104,12 @@ else{
               </p>
             </a>
             <ul class="nav nav-treeview">
-              <li class="nav-item">
+              <!--li class="nav-item">
                 <a href="profile.php" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Update Profile</p>
                 </a>
-              </li>
+              </li-->
               <li class="nav-item">
                 <a href="changepassword.php" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
