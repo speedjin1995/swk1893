a72de4a4e702fd5238faf4de6ccbd72b7392ff9d
speedjin1995 <speedjin1995@gmail.com>
2022-02-22 19:23:44 +0800

    done testimony
    done testimony

--- testimony.php
+++ testimony.php
@@ -24,6 +24,8 @@ if(!isset($_SESSION['language'])){
 else{
     $language = $_SESSION['language'];
 }
+
+$products = $db->query("SELECT * FROM testimony");
 ?>
 <html lang="en">
     <head>
@@ -142,7 +144,7 @@ else{
             <section class="uk-section uk-text-center uk-flex uk-flex-middle uk-flex-center uk-light uk-background-cover uk-background-top-center"
             data-overlay="0.4" style="background-color: #151515; background-image: url(assets/demo/page-bg.jpg); height: 500px;">
                 <div class="tw-page-title-container tw-element">
-                    <h1 class="tw-page-title uk-text-uppercase">Services</h1>
+                    <h1 class="tw-page-title uk-text-uppercase"><?=$languageArray['page_testimony'][$language] ?></h1>
                 </div>
                 <!--<div class="tw-breadcrumb-container uk-position-absolute uk-position-bottom-center tw-element">
                     <a href="index.php" class="tw-breadrumb-child home">Home</a>
@@ -153,93 +155,38 @@ else{
             <section class="uk-section">
                 <div class="uk-container">
                     <div class="tw-element tw-heading uk-text-center">
-                        <h3>
-                            Why choose us ?
-                        </h3>
-                        <p class="subtitle">
-                            Sandalwood tea is the latest trend. 
-                        </p>
+                        <h3><?=$languageArray['testimony_why_choose_us'][$language] ?></h3>
+                        <p class="subtitle"><?=$languageArray['testimony_why_choose_us2'][$language] ?></p>
                     </div>
                     
                     <div class="tw-element uk-child-width-1-1 uk-child-width-1-3@m uk-child-width-1-2@s" data-uk-grid data-uk-scrollspy="target: > div; cls:uk-animation-slide-bottom-medium; delay: 300;">
-                        
                         <div>
                             <div class="tw-element tw-box small-typography layout-2">
                                 <i class="ion-leaf"></i>
-                                <h4>
-                                    Antioxidant
-                                </h4>
-                                <p class="description">
-                                    Duis sed odio sit amet nibh vulputate cursus sit amet mauris morbi accumsan ipsum velit erot auctor ornare lorem ipsum odio.
-                                </p>
+                                <h4><?=$languageArray['testimony_antioxidant'][$language] ?></h4>
+                                <p class="description"><?=$languageArray['testimony_antioxidant2'][$language] ?></p>
                             </div>
                         </div>
                         
                         <div>
                             <div class="tw-element tw-box small-typography layout-2">
                                 <i class="ion-leaf"></i>
-                                <h4>
-                                    Tannin
-                                </h4>
-                                <p class="description">
-                                    Duis sed odio sit amet nibh vulputate cursus sit amet mauris morbi accumsan ipsum velit erot auctor ornare lorem ipsum odio.
-                                </p>
+                                <h4><?=$languageArray['testimony_tannin'][$language] ?></h4>
+                                <p class="description"><?=$languageArray['testimony_tannin2'][$language] ?></p>
                             </div>
                         </div>
                         
                         <div>
                             <div class="tw-element tw-box small-typography layout-2">
                                 <i class="ion-leaf"></i>
-                                <h4>
-                                    Peace of mind
-                                </h4>
-                                <p class="description">
-                                    Duis sed odio sit amet nibh vulputate cursus sit amet mauris morbi accumsan ipsum velit erot auctor ornare lorem ipsum odio.
-                                </p>
+                                <h4><?=$languageArray['testimony_peace_of_mind'][$language] ?></h4>
+                                <p class="description"><?=$languageArray['testimony_peace_of_mind2'][$language] ?></p>
                             </div>
                         </div>
-                        
-                        <!--<div>
-                            <div class="tw-element tw-box small-typography layout-2">
-                                <i class="ion-leaf"></i>
-                                <h4>
-                                    Fully responsive
-                                </h4>
-                                <p class="description">
-                                    Duis sed odio sit amet nibh vulputate cursus sit amet mauris morbi accumsan ipsum velit erot auctor ornare lorem ipsum odio.
-                                </p>
-                            </div>
-                        </div>
-                        
-                        <div>
-                            <div class="tw-element tw-box small-typography layout-2">
-                                <i class="ion-leaf"></i>
-                                <h4>
-                                    Graphic Design
-                                </h4>
-                                <p class="description">
-                                    Duis sed odio sit amet nibh vulputate cursus sit amet mauris morbi accumsan ipsum velit erot auctor ornare lorem ipsum odio.
-                                </p>
-                            </div>
-                        </div>
-                        
-                        <div>
-                            <div class="tw-element tw-box small-typography layout-2">
-                                <i class="ion-leaf"></i>
-                                <h4>
-                                    Friendly Support
-                                </h4>
-                                <p class="description">
-                                    Duis sed odio sit amet nibh vulputate cursus sit amet mauris morbi accumsan ipsum velit erot auctor ornare lorem ipsum odio.
-                                </p>
-                            </div>
-                        </div>-->
-                        
                     </div>
-                    
                 </div>
             </section>
-            <section class="uk-section uk-light" data-uk-parallax="bgy: -200" data-overlay="0.5" style="background-color: #151515; background-image: url(assets/demo/process-bg.jpg);">
+            <!--section class="uk-section uk-light" data-uk-parallax="bgy: -200" data-overlay="0.5" style="background-color: #151515; background-image: url(assets/demo/process-bg.jpg);">
                 <div class="uk-container">
                     <div class="tw-process uk-text-center tw-element uk-child-width-expand@l uk-child-width-1-2@s normal" data-uk-grid data-uk-scrollspy="target: > div; cls:uk-animation-slide-right-medium; delay: 300;">
                         <div class="tw-process-block uk-padding-large">
@@ -272,43 +219,36 @@ else{
                         </div>
                     </div>
                 </div>
-            </section>
-            <section class="uk-section uk-section-small">
+            </section-->
+            <section class="uk-section uk-section-shop">
                 <div class="uk-container">
-                    <div class="uk-child-width-1-2@m uk-flex-middle" data-uk-grid>
-                        <div>
-                            <div class="tw-element tw-accordion">
-                                <ul data-uk-accordion="collapsible: false;">
-                                    <li>
-                                        <h3 class="uk-accordion-title">Accordion first</h3>
-                                        <div class="uk-accordion-content">
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <h3 class="uk-accordion-title">Accordion second</h3>
-                                        <div class="uk-accordion-content">
-                                            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor reprehenderit.</p>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <h3 class="uk-accordion-title">Accordion third</h3>
-                                        <div class="uk-accordion-content">
-                                            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat proident.</p>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <h3 class="uk-accordion-title">Accordion fourth</h3>
-                                        <div class="uk-accordion-content">
-                                            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat proident.</p>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div>
-                            <div class="tw-element">
-                                <img src="assets/demo/pages/page-service.jpg" alt="" />
+                    <div data-uk-grid>
+                        <div class="content-area uk-width-expand">
+                            <div class="tw-element tw-shop">
+                                <div class="shop-container uk-child-width-1-1@xxs uk-child-width-1-2@xs uk-child-width-1-3@s uk-child-width-1-2@m uk-child-width-1-3@l" data-uk-grid data-uk-scrollspy="target: > .shop-item; cls:uk-animation-slide-bottom-medium; delay: 300;">
+                                    <?php while($row=mysqli_fetch_assoc($products)){
+                                        echo '<div>
+                                            <div class="shop-item">
+                                                <div class="shop-content">
+                                                    <div class="shop-image-container uk-inline tw-onhover visible">
+                                                        <img alt="Backpack In Black" src="admin/php/'.$row['product_photo'].'">
+                                                        <div class="uk-position-bottom">
+                                                            <a class="add-to-cart" onclick="edit('.$row['id'].')">More Info &nbsp;<i class="icon ion-arrow-right-a"></i></a>
+                                                        </div>
+                                                    </div>';
+                                                    
+                                        if($language == "ch"){
+                                            echo '<h4 onclick="edit('.$row['id'].')">'.$row['product_name_ch'].'</h4>';
+                                        }
+                                        else{
+                                            echo '<h4 onclick="edit('.$row['id'].')">'.$row['product_name'].'</h4>';
+                                        }
+
+                                        echo '</div>
+                                            </div>
+                                        </div>';
+                                    } ?>
+                                </div>
                             </div>
                         </div>
                     </div>
